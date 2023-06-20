a09b5afc74544a1c42bb4eb74ff379ebf2b265be
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2023-06-20 11:20:11 -0700

    Create success.php

---
+++ success.php
@@ -0,0 +1,21 @@
+<?php
+include("includes/header.php");
+?>
+
+
+<div class="wrapper">
+	
+	<div class="content cv-align-contents">
+		<div class="card" id="login">
+			<h1>Welcome</h1>
+		<div class="btn-secondary">
+					<a href="login.php">Login</a>
+					
+				</div><!-- end of cta -->
+			</div><!-- end of card -->
+		</div><!-- end of content -->
+
+	</div><!-- end of wrapper -->
+
+</body>
+</html>
